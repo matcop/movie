0d8bd44b1dc88672b00ef926eecf90650b4e65f6
matcop <matcop@gmail.com>
2020-12-03 15:43:42 -0400

    96 inicio proyecto  peliculas

---
+++ lib/src/pages/home_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomePages extends StatelessWidget {
+  const HomePages({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+      child: Text('este es el home Page peliculas'),
+    ));
+  }
+}
